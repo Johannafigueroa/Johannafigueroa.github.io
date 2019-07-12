c31ec23e9426e4dd24ee1cb93aa391086075ee0e
HoangNguyen0524 <40507081+HoangNguyen0524@users.noreply.github.com>
2019-07-12 13:54:08 -0700

    Update sketch.js

--- Final-Project2/sketch.js
+++ Final-Project2/sketch.js
@@ -1,4 +1,8 @@
+let img;
+
 function setup() {
+  createCanvas(720, 400);
+  img = loadImage('e4d769742a76da9dba84a36ca541413f.jpg');
   var canvas = createCanvas(800, 400);
   canvas.parent('patrick-stump');
    noStroke();
@@ -10,6 +14,10 @@ function draw()
     if (mouseX > width/1.1) {
       background('black');
      drawAJR();
+    image(img, 0, 0, img.width / 4, img.height / 4);
+    noStroke();
+    fill('red');
+    ellipse(10,10,10,10);
 }
 
 function drawCircle()
