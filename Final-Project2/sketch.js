1251caa5eef043ff2eaf88a837e27f3e858d4f30
HoangNguyen0524 <40507081+HoangNguyen0524@users.noreply.github.com>
2019-07-13 19:24:05 -0700

    Update sketch.js

--- Final-Project2/sketch.js
+++ Final-Project2/sketch.js
@@ -1,1 +1,16 @@
+let img;
 
+function setup() {
+  createCanvas(720, 400);
+  img = loadImage('bru.jpg');
+}
+
+function draw() {
+  mapoof();
+}
+function mapoof(){
+	image(img, 0, 0, img.width / 4, img.height / 4);
+  noStroke();
+  fill('red');
+  ellipse(10,10,10,10);
+}
