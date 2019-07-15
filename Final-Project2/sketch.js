f3c9f970a949c4d74ea1efb62b65cf872af772f5
HoangNguyen0524 <40507081+HoangNguyen0524@users.noreply.github.com>
2019-07-14 17:41:46 -0700

    Update sketch.js

--- Final-Project2/sketch.js
+++ Final-Project2/sketch.js
@@ -12,14 +12,24 @@ function mapoof(){
   image(img, 0, 0, img.width / 4, img.height / 4);
   noStroke();
   fill('red');
+  //Indiana
   ellipse(370,140,10,10);
-  ellipse(470,100,10,10);
-  ellipse(480,100,10,10);
+  //Vermont
+  ellipse(485,75,10,10);
+  //New Hampshire
+  ellipse(490,75,10,10);
+  //West Virginia
   ellipse(390,145,10,10);
+  //Connecticut
   ellipse(10,10,10,10);
+  //Wisconsin
   ellipse(10,10,10,10);
+  //Rhode Island
   ellipse(10,10,10,10);
+  //Ohio
   ellipse(10,10,10,10);
+  //Pennsylvania
   ellipse(10,10,10,10);
+  //New York
   ellipse(10,10,10,10);
 }
