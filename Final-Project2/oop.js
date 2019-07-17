06905ad3caa00652794dcb161157882a2587de29
Girlcoding22 <31711391+Girlcoding22@users.noreply.github.com>
2019-07-17 14:20:10 -0700

    Update oop.js

--- Final-Project2/oop.js
+++ Final-Project2/oop.js
@@ -1,7 +1,8 @@
-function setup{
-createcanvas(720,420);
+function setup(){
+  createCanvas(720,420);
   background('green');
 }
+
 function draw(){
-text('what up bro');
+ellipse(56, 46, 55, 55);
 }
